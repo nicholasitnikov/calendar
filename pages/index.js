d0aec8cc9ce25cd5fa2caa25967546de62f087eb
Nikolay Sitnikov <nicholasitnikov@gmail.com>
2022-01-04 11:59:08 +0500

    feat: add favicon

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import styles from '../styles/index.module.css';
 import { v4 as uuidv4 } from 'uuid';
 
 import data from '../utils/data';
+import Head from 'next/head';
 
 export default function Home() {
 
@@ -43,8 +44,27 @@ export default function Home() {
 
   return (
     <>
+      <Head>
+        <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png" />
+        <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png" />
+        <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png" />
+        <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png" />
+        <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png" />
+        <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png" />
+        <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png" />
+        <link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
       <header className={styles.header}>
-        <h1 className={styles.headingOne}>Jane&rsquo;s calendar 🤑</h1>
+        <h1 className={styles.headingOne}>🤑 Money calendar 🤑</h1>
         <h2 className={styles.headingTwo}>{`Накоплено всего: ${calculateTotal()} ₽`}</h2>
       </header>
       <main className={styles.cells}>
